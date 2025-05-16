b2313578fda0de0a00b2ae6038d0598c30489e51
System administrator <root@nixos-wsl.>
2025-05-16 14:48:47 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "NixOS WSL Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, ...}@inputs: {
+    nixosConfigurations.nixos-wsl = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        nixos-wsl.nixosModules.wsl
+
+        ./modules/configuration.nix
+
+        ({ pkgs, lib, ...}: {
+          wsl = {
+            enable = true;
+            defaultUser = "derek";
+            startMenuLaunchers = true;
+            wslConf.network.generateResolvConf = false;
+          };
+        })
+      ];
+    };
+  };
+}
---
+++ modules/configuration.nix
@@ -0,0 +1,57 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>  
+  ];
+    networking = {
+	hostName = "nixos-wsl";
+	nameservers = [ "8.8.8.8" "8.8.4.4" ];
+    };
+
+    nix = {
+      settings = {
+	experimental-features = [ "nix-command" "flakes" ];
+	substituters = [ 
+	  "https://cache.nixos.org"
+	];
+	trusted-public-keys = [
+	  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="	
+	];
+      };
+    };
+
+    security.pki.certificates = [
+	(builtins.readFile "/home/derek/certs/ZscalerRootCertificate-2048-SHA256.crt")
+    ];
+
+    environment.systemPackages = with pkgs; [
+	git
+	vim
+	wget
+	curl
+	htop
+    ];
+
+    time.timeZone = "America/New_York";
+
+    users = {
+	groups = {
+	  nixconfig = {};
+	};
+	users = {
+	  derek = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" "nixconfig" ];
+		initialPassword = "password";
+	  };
+	};	
+    };
+
+    systemd.tmpfiles.rules = [
+	"d /etc/nixos 0775 root nixconfig - -"
+    ];
+
+    system.stateVersion = "24.11"; # Did you read the comment?
+}
